6aa9a13ad653e7c781f11c642af643e46d959fe6
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2023-02-01 17:08:23 +0100

    CRUD Benevole OK

--- routes/benevoles/routes.js
+++ routes/benevoles/routes.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const Benevole = require("../../models/Benevole");
 
-router.get('/by_zone', (req, res)=>{
-    res.send("on no");
+
+/**
+ * Route getting every benevole assigned to a specified zone
+ * ordered by hours
+ */
+router.get('/by_zone/:zoneId', async (req, res)=>{
+    try{
+        const benevoles = await Benevole.find(); //TODO add query pour la zone
+        res.json(benevoles);
+    } catch (err){
+        res.status(500).json({message: err});
+    }
 });
 
 /**
@@ -15,6 +25,18 @@ router.get("/", async (req, res)=>{
         res.json(benevoles);
     } catch (err){
         res.status(500).json({message: err});
+    }
+})
+
+/**
+ * Route getting a benevole by a specified ID
+ */
+router.get("/:beneId", async(req, res)=>{
+    try{
+        const benevole = await Benevole.findById(req.params.beneId);
+        res.json(benevole);
+    }catch (err){
+        res.status(404).json({message: err});
     }
 })
 
@@ -32,6 +54,33 @@ router.post('/', async(req, res)=>{
         res.status(200).json(savedBenevole);
     } catch (err){
         res.status(500).json({message: err})
+    }
+})
+
+/**
+ * Route updating a single benevole by its id
+ */
+router.patch('/:beneId', async (req, res)=>{
+    try {
+        const updatedBenevole = await Benevole.updateOne(
+            {_id: req.params.beneId},
+            {$set: {nom: req.body.nom, prenom: req.body.prenom, email: req.body.email}}
+        );
+        res.json(updatedBenevole);
+    }catch (err){
+        res.status(404).json({message:err});
+    }
+})
+
+/**
+ * Route deleting a benevole by its id
+ */
+router.delete('/:beneId', async (req, res)=>{
+    try{
+        const removedBenevole = await Benevole.remove({_id: req.params.beneId});
+        res.json(removedBenevole);
+    } catch (err){
+        res.status(404).json({message: err});
     }
 })
 
