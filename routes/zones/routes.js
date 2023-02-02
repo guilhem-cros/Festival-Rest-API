14e4db236e70b3bea43316d30472c306a4a0ad27
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2023-02-02 15:33:51 +0100

    Node start

--- routes/zones/routes.js
+++ routes/zones/routes.js
@@ -13,23 +13,49 @@ router.get("/", async (req, res)=>{
         res.status(500).json({message: err});
     }
 })
-/*
+
+/**
+ * Route used to add a benevole to a specified zone
+ */
 router.patch("/addBenevoleTo/:zoneId", async (req, res)=>{
     try{
         const toUpdateZone = await Zone.findById(req.params.zoneId);
-        toUpdateZone.push
+        const benevoles = toUpdateZone.benevoles;
+        benevoles.push({heureDebut: req.params.heureDebut, heureFin: req.params.heureFin, benevole:req.params.benevole});
         const updatedZone = await Zone.updateOne(
             {_id: req.params.zoneId},
             {
                 $set:{
-                    benevoles:
+                    benevoles:benevoles
                 }
             }
         )
+        res.json(updatedZone);
     }catch (err){
         res.status(404).json({message : err});
     }
 })
-*/
+
+router.patch("/addJeuTo/:zoneId", async (req, res)=>{
+    try{
+        const toUpdateZone = await Zone.findById(req.params.zoneId);
+        const jeux = toUpdateZone.jeux;
+        jeux.push(req.params.jeu);
+        const updatedZone = await Zone.updateOne(
+            {_id: req.params.zoneId},
+            {
+                $set:{
+                    jeux:jeux
+                }
+            }
+        )
+        res.json(updatedZone);
+    }catch (err){
+        res.status(404).json({message : err});
+    }
+})
+
+
+
 
 module.exports = router;
