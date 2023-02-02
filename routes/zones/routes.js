f2770fbe17fbed15a77fb7dedc251e24c76b18fa
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2023-02-02 15:19:45 +0100

    Cors

--- routes/zones/routes.js
+++ routes/zones/routes.js
@@ -13,5 +13,23 @@ router.get("/", async (req, res)=>{
         res.status(500).json({message: err});
     }
 })
+/*
+router.patch("/addBenevoleTo/:zoneId", async (req, res)=>{
+    try{
+        const toUpdateZone = await Zone.findById(req.params.zoneId);
+        toUpdateZone.push
+        const updatedZone = await Zone.updateOne(
+            {_id: req.params.zoneId},
+            {
+                $set:{
+                    benevoles:
+                }
+            }
+        )
+    }catch (err){
+        res.status(404).json({message : err});
+    }
+})
+*/
 
 module.exports = router;
