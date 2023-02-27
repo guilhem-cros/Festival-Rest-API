cd327e5c998a40b6effb5fa5e8d2bbbc2602ae6a
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2023-02-27 10:29:08 +0100

    firebase middleware

---
+++ config/firebase-config.js
@@ -0,0 +1,8 @@
+const admin = require("firebase-admin");
+const credential = require("./serviceAccount.json");
+
+admin.initializeApp({
+    credential : admin.credential.cert(credential)
+});
+
+module.exports = admin;
