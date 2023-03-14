8821d8713e78190f85d8a74216dcf749bb751ff0
guilhem-cros <guilhem.cros01@etu.umontpellier.fr>
2023-03-14 10:22:18 +0100

    Pb benev ok

--- routes/benevoles/routes.js
+++ routes/benevoles/routes.js
@@ -35,14 +35,12 @@ router.get("/:beneId", async(req, res)=>{
 router.get("/:beneId/zones", async(req, res)=>{
     try{
         const zones = await Zone.find({},{ jeux:0});
-        const benevole = await Benevole.findById(req.params.beneID);
+        const searchedBenev = await Benevole.findById(req.params.beneId);
         const creneauxBenev = [];
         for (const zone of zones) {
             for (const benev of zone.benevoles) {
                 if (benev.benevole.localeCompare(req.params.beneId) == 0) {
-
-                    console.log(zone.nom)
-                    creneauxBenev.unshift({"benevole": benevole,"zoneId": zone._id, "zoneName": zone.nom, "heureDebut": benev.heureDebut, "heureFin": benev.heureFin})
+                    creneauxBenev.unshift({"benevole": searchedBenev,"zoneId": zone._id, "zoneName": zone.nom, "heureDebut": benev.heureDebut, "heureFin": benev.heureFin})
                 }
             }
         }
